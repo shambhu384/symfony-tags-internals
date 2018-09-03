1bfd5a91c1d623ab5406957db95af1f3aafb84ab
Shambhu Kumar <shambhu.kumar@fridaymediagroup.com>
2018-09-03 09:17:04 -0400

    Second args added

--- src/LowCalculation.php
+++ src/LowCalculation.php
@@ -6,9 +6,15 @@ use App\TaxCalculationInterface;
 
 class LowCalculation implements TaxCalculationInterface
 {
+    private $min_limit;
+
+    public function __construct($min_limit) {
+        $this->min_limit = $min_limit;
+    }
+
     public function apply(int $amount): int
     {
-        if ( $amount < 150000) {
+        if ( $amount < $this->min_limit) {
             throw new \InvalidArgumentException(sprintf("%s amount is not coming under taxable \n", $amount));
         }
 
--- src/TaxPPFDeduction.php
+++ src/TaxPPFDeduction.php
@@ -7,16 +7,23 @@ use App\TaxCalculationInterface;
 class TaxPPFDeduction implements TaxCalculationInterface
 {
     private $intrest;
+    private $plan;
 
-    public function __construct(int $intrest)
+    public function __construct(int $intrest, int $plan)
     {
         $this->intrest = $intrest;
+        $this->plan = $plan;
     }
 
     public function apply(int $amount): int
     {
-        $rate = $amount / 100 * $this->intrest;
+        if($this->plan > 2000000) {
+            $amount = $amount / 100 * $this->intrest;
+        } else {
+            $rate = $amount / 100 * $this->intrest;
+            $amount = ($amount - $rate) + 1400;
+        }
 
-        return $amount - $rate;
+        return $amount;
     }
 }
