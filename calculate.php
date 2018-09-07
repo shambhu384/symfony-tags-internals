777abe9d8fc09349a78f8011bc9bbd90cb7c84d2
Shambhu Kumar <shambhu.kumar@fridaymediagroup.com>
2018-09-07 05:55:02 -0400

    Symfony container, config added

--- calculate.php
+++ calculate.php
@@ -2,12 +2,24 @@
 
 use Container\Container;
 
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use App\DependencyInjection\TaxManagerCompilerPass;
+
 require 'vendor/autoload.php';
 
-$container = Container::instance();
+$containerBuilder = new ContainerBuilder();
 
-$manager = $container->get('tax_manager');
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+$loader->load('services.yml');
 
+$containerBuilder->addCompilerPass(new TaxManagerCompilerPass());
+$containerBuilder->compile();
+
+
+$manager = $containerBuilder->get('tax_manager');
 try {
     $manager->apply($argv[1]);
 } catch(Exception $e) {
